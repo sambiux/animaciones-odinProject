7539383d2e37758148252c39b2c855d33c4cde6a
sambiux <sddaza.9@gmail.com>
2025-12-17 22:56:17 +0000

    Agregando archivos

---
+++ service/main.js
@@ -0,0 +1,6 @@
+// Make the ball move to a certain position:
+const f = document.getElementById("foo");
+document.addEventListener("click", (ev) => {
+  f.style.transform = `translateY(${ev.clientY - 25}px)`;
+  f.style.transform += `translateX(${ev.clientX - 25}px)`;
+});
